dcb219e290cdeed39bee7eb2302b8cc0b73269b9
cherkasskypavel <fenderik09@mail.ru>
2023-08-31 16:57:14 +0300

    Добавил в conftest функцию по добавлению параметра выбора браузера к запуску тестов; Добавил модуль pytest-rerunfailures;

--- section3/conftest.py
+++ section3/conftest.py
@@ -1,14 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
 
 
-@pytest.fixture(scope="session")
-def browser():
-    print('\nОткрываем браузер')
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(1)
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print('\nЗакрываем браузер')
-    browser.close()
-
+    print("\nquit browser..")
+    browser.quit()
